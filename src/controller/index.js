5d95a938fcf444a3fcbdf74d2e9abad67e708f95
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2024-04-29 19:04:44 -0500

    Delete src directory

--- src/controller/index.js
+++
@@ -1,8 +0,0 @@
-import { Activity } from "../model";
-
-export const fetchData = async () => {
-  const res = await fetch('https://www.boredapi.com/api/activity/');
-  const data = await res.json()
-
-  return Activity.serialize(data);
-}
